9c16122edf8f27bacdf7115e7ce5848672bad9c6
aha <ntuaha@gmail.com>
2024-05-21 13:40:39 +0800

    init

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {"key":"a","name": "林鉦育", "site": 1},
+    {"key":"b","name": "正庭", "site": 2},
+    {"key":"c","name": "張杰", "site": 3},
+    {"key":"d","name": "男州懂", "site": 4},
+  ];
